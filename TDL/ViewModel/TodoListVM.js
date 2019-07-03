4fce69c1c19b3b68684ac82c9f6962ed85eea638
Andy Rosete <andy.rosete@innovation.group>
2019-07-03 19:25:00 +0200

    Add project files.

---
+++ TDL/ViewModel/TodoListVM.js
@@ -0,0 +1,24 @@
+﻿function ToDo(stuff) {
+    this.toDoItem = stuff;
+}
+
+function ToDoViewModel() {
+    var self = this;
+
+    self.toDoItems = ko.observableArray([
+        new ToDo("Watch Person of Interest"),
+        new ToDo("Study for Midterm exam"),
+        new ToDo("Buy groceries for Luis")
+    ]);
+
+    self.addToDoItem = function () {
+        self.toDoItems.push(new ToDo($('.txt').val()));
+        $('.txt').val('');
+    };
+
+    this.removeToDoItem = function (item) {
+        self.toDoItems.remove(item);
+    };
+}
+
+ko.applyBindings(new ToDoViewModel());
