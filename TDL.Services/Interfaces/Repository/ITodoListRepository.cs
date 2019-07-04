2f72221f0fc88f18491d490dd5f7637bb0f6ecf5
Andy Rosete <andy.rosete@innovation.group>
2019-07-04 22:42:43 +0200

    Add service project, update TodoListRepository

---
+++ TDL.Services/Interfaces/Repository/ITodoListRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace TDL.Services.Interfaces.Repository
+{
+    using System;
+    using TDL.Common.Interfaces;
+    using TDL.Services.Models;
+
+    public interface ITodoListRepository<TEntity>
+         where TEntity : IEntity
+    {
+
+        Response GetAll();
+
+        Response Get(Guid id);
+
+        Response Get(int id);
+
+        Response Create(Action<TEntity> setupProperty);
+
+        Response Create();
+
+        Response CommitContextChanges();
+
+        Response RemoveData(Guid id);
+    }
+}
