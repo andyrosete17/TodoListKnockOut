45199beaa29a6e111889f1de2a0bbfe859d67a1d
Andy Rosete <andy.rosete@innovation.group>
2019-07-05 01:02:56 +0200

    Added service methods and repository

---
+++ TDL.Services/ITodoListService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using TDL.Services.Models;
+
+namespace TDL.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ITodoListService" in both code and config file together.
+    [ServiceContract]
+    public interface ITodoListService
+    {
+        [OperationContract]
+        Response GetAllData();
+
+        [OperationContract]
+        Response RemoveData(Guid id);
+
+        [OperationContract]
+        Response ChangeStatus(Guid id, string status);
+    }
+}
---
+++ TDL.Services/TodoListService.svc.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using TDL.Common.Enums;
+using TDL.Domain;
+using TDL.Services.Interfaces.Repository;
+using TDL.Services.IOCRegistry;
+using TDL.Services.Models;
+using Unity;
+using Unity.Resolution;
+
+namespace TDL.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "TodoListService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select TodoListService.svc or TodoListService.svc.cs at the Solution Explorer and start debugging.
+    public class TodoListService : ITodoListService
+    {
+        private readonly ITodoListRepository<ToDoListItems> _repository;
+        private readonly LocalDataContext localDataContext;
+
+        public TodoListService()
+        {
+            localDataContext = new LocalDataContext();
+            ///IOC register initialization
+            TodoListServiceRegistry.RegisterComponents();
+            _repository = TodoListServiceRegistry.container.Resolve<ITodoListRepository<ToDoListItems>>(new ResolverOverride[]
+                                                            {
+                                                                new ParameterOverride("dataContext", localDataContext)
+                                                            });
+
+        }
+
+        public TodoListService(ITodoListRepository<ToDoListItems> repository, LocalDataContext _localDataContext)
+        {
+            localDataContext = _localDataContext;
+            ///IOC register initialization
+            TodoListServiceRegistry.RegisterComponents();
+            _repository = repository;
+        }
+
+        public Response ChangeStatus(Guid id, string status)
+        {
+            var item = _repository.Get(id);
+            var result = item.Result as ToDoListItems;
+            result.Status = (ToDoListStatusEnum)Enum.Parse(typeof(ToDoListStatusEnum), status);
+            _repository.CommitContextChanges();
+            item.Result = result;
+            return item;
+        }
+
+        public Response GetAllData()
+        {
+            return _repository.GetAll();
+        }
+
+        public Response RemoveData(Guid id)
+        {
+            var result = _repository.RemoveData(id);
+            _repository.CommitContextChanges();
+
+            return result;
+        }
+    }
+}
