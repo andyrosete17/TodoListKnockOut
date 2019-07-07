cb9390b35ca77967cf1f7431df4426693a7cd661
Andy Rosete <andy.rosete@innovation.group>
2019-07-07 22:24:06 +0200

    Fix project root

--- TDL.Domain/DataContext.cs
+++ TDL.Domain/DataContext.cs
@@ -1,7 +1,6 @@
 ﻿namespace TDL.Domain
 {
     using System.Data.Entity;
-
     public class DataContext : DbContext
     {
         public DataContext() : base("DefaultConnection")
--- TDL.Tests/Controllers/HomeControllerTest.cs
+++ TDL.Tests/Controllers/HomeControllerTest.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TDL;
 using TDL.Controllers;
 
 namespace TDL.Tests.Controllers
